370f7adfd8ae07cf1b9fe590db984c5e38b887b8
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-07-09 23:23:08 +0530

    banner

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Banner from './Banner';
 import './HomeScreen.css';
 import Nav from './Nav';
 
@@ -7,7 +8,7 @@ function HomeScreen() {
         <div className="homeScreen">
             <Nav />
 
-            {/* Banner */}
+            <Banner />
 
             {/* Row */}
         </div>
--- src/Nav.js
+++ src/Nav.js
@@ -20,7 +20,7 @@ function Nav() {
     return (
         <div className={`nav ${show && "nav__black"}`}>
             <div className="nav__contents">
-                <img className="nav__logo" src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" /> 
+                <img className="nav__logo" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="" /> 
                 <img className="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="" />
             </div>
             
