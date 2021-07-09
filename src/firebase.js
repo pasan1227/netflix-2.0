310d082740339b035d0cd2eddcf82180fae2f0ca
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-07-09 10:25:09 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCoxjuY9VVL8HQG4g_cD1VsNyCjjdl0Qh4",
+    authDomain: "netflix-2-0.firebaseapp.com",
+    projectId: "netflix-2-0",
+    storageBucket: "netflix-2-0.appspot.com",
+    messagingSenderId: "492873726816",
+    appId: "1:492873726816:web:b62247cb563fb660953c05"
+  };
