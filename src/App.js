8bbeb5a0ff089298c0375d2bd3a6d55153de3ed4
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-07-09 10:34:37 +0530

    Home Screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './HomeScreen';
 
 function App() {
   return (
     <div className="app">
-      <h1>Netflix 2.0</h1>
+      <HomeScreen />
     </div>
   );
 }
---
+++ src/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomeScreen.css';
+
+function HomeScreen() {
+    return (
+        <div className="homeScreen">
+            {/* NAV*/}
+
+            {/* Banner */}
+
+            {/* Row */}
+        </div>
+    )
+}
+
+export default HomeScreen
