7ab4dcc740d769f8af06b9c0deb0846dfc2a4f94
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-07-09 13:06:03 +0530

    nav bar added

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './HomeScreen.css';
+import Nav from './Nav';
 
 function HomeScreen() {
     return (
         <div className="homeScreen">
-            {/* NAV*/}
+            <Nav />
 
             {/* Banner */}
 
--- src/features/counterSlice.js
+++ src/features/counterSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './counter/counterAPI';
+import { fetchCount } from './counterAPI';
 
 const initialState = {
   value: 0,
