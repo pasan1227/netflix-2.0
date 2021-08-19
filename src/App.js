30571b88d1f0881fce3f45a19c3124dd60a8c0e3
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-08-19 12:17:50 +0530

    login screen

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import LoginScreen from './screens/LoginScreen';
 
 function App() {
-  return (
+  const user = null;
+
+  return ( 
     <div className="app">
-      
       <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+        {!user ? (
+          <LoginScreen />
+        ): (
+          <Switch>
+            <Route exact path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
+        
       </Router>
     </div>
   );
