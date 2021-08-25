4ea35dff2413d4c01056919543e7876284ea3f0a
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-08-25 10:50:37 +0530

    implementing redux

--- src/App.js
+++ src/App.js
@@ -4,36 +4,44 @@ import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         // Logged in
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
         // Logged out
+        dispatch(logout);
       }
     });
 
     return unsubscribe;
   }, []);
 
-  return ( 
-    <div className="app">
+  return (
+    <div className='app'>
       <Router>
         {!user ? (
           <LoginScreen />
-        ): (
+        ) : (
           <Switch>
-            <Route exact path="/">
+            <Route exact path='/'>
               <HomeScreen />
             </Route>
           </Switch>
         )}
-        
       </Router>
     </div>
   );
