d987fd4ee0c681b9a30ec79967d868a0039be3ba
Pasan Ratnayake <pasanratnayake@gmail.com>
2021-08-25 10:09:02 +0530

    setting up authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCoxjuY9VVL8HQG4g_cD1VsNyCjjdl0Qh4",
     authDomain: "netflix-2-0.firebaseapp.com",
@@ -6,3 +8,10 @@ const firebaseConfig = {
     messagingSenderId: "492873726816",
     appId: "1:492873726816:web:b62247cb563fb660953c05"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth };
+  export default db;
